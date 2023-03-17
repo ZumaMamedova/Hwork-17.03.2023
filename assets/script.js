dd0825cd8b09a95bc9126c8ae5750682c81134d3
ZumaMamedova <zumrudam@code.edu.az>
2023-03-17 22:19:36 +0400

    html

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+//let a =5;
+
+//console.log(a);
+
+// localStorage.setItem("num",5);
+// localStorage.clear();
+// console.log(localStorage.getItem("num"));
+
+// localStorage.setItem("name","lorem");
+// localStorage.setItem("surname","ipsumov");
+
+//localStorage.setItem("age",20);
+//localStorage.clear();
+//localStorage.removeItem("name");
+
+// let numbers=[1,2,3,4];
+// localStorage.setItem("numberArr",JSON.stringify(numbers));
+// console.log(localStorage.getItem("numberArr"));
+
+// let user={
+//     name: "lorem",
+//     surname:"ipsum",
+//     age:20,
+// };
+// localStorage.setItem("user",JSON.stringify(user));
+// console.log(JSON.parse(localStorage.getItem("user")));
+
+
+
+
